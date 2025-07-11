71f2324a1fc86d51763b9788353d5e4c0b196d31
younaJ00 <you.na.joo0@gmail.com>
2025-07-11 15:05:04 +0900

    chore: commitlint 추가

---
+++ commitlint.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			['feat', 'fix', 'chore', 'docs', 'refactor'],
+		],
+		'subject-case': [2, 'never', ['start-case', 'pascal-case']],
+	},
+}
