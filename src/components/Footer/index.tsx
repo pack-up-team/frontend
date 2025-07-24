cdd7259322e115466e61d83d51464a372f9ed6c6
codenamewont <codenamewont@gmail.com>
2025-07-25 01:28:45 +0900

    feat: footer 컴포넌트 모바일 및 태블릿 구현

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,10 +4,10 @@ import { LogoIcon } from '../../assets';
 
 const Footer: React.FC = () => {
     return (
-        <footer className='flex w-full pt-6 pb-14 flex-col items-start gap-6 border-t border-[#F0F0F0]'>
-            <div className='w-[1200px] mx-auto flex justify-between items-center self-stretch'>
+        <footer className='flex flex-col items-center gap-6 w-full px-4 pt-6 pb-[42px] tablet:px-[60px] web:px-[120px] web:pb-14 web:items-start border-t border-[#F0F0F0]'>
+            <div className='w-full max-w-[1200px] mx-auto flex flex-col items-center gap-4 self-stretch web:flex-row web:justify-between'>
                 {/* 좌측: 로고 + 링크 */}
-                <div className='flex w-[390px] items-center gap-2'>
+                <div className='flex items-center gap-2'>
                     <LogoIcon className="w-8 h-8" />
                     <div className='flex items-center'>
                         <a href='#' target='_blank' rel='noopener' className='flex h-11 px-6 justify-center items-center gap-2 text-[#949494] font-pretendard text-base font-medium leading-[140%] hover:underline hover:text-[#707070]'>이용약관</a>
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
                     </div>
                 </div>
                 {/* 우측: 카피라이트 */}
-                <div className='text-[#CCC] text-right font-montserrat text-base font-medium leading-[normal]'>Copyright © PACKUP Co. All rights reserved</div>
+                <div className='self-stretch text-[#CCC] text-center font-montserrat text-base font-medium leading-[normal] tablet:self-auto tablet:text-right'>Copyright © PACKUP Co. All rights reserved</div>
             </div>
         </footer>
     );
