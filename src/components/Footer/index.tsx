eede880883ea2614cf4ee84af04095d709376e05
codenamewont <codenamewont@gmail.com>
2025-07-24 19:11:12 +0900

    fix: footer 컴포넌트 반응형 반영 웹 화면 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,30 +4,24 @@ import { LogoIcon } from '../../assets';
 
 const Footer: React.FC = () => {
     return (
-        <footer className='flex w-[1520px] pb-[56px] flex-col items-start gap-[24px] mx-auto'>
-            <div className='h-0 self-stretch'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1521" height="2" viewBox="0 0 1521 2" fill="none">
-                    <path d="M0.904297 0.582275H1520.9" stroke="#F0F0F0" strokeWidth="1" />
-                </svg>
-            </div>
-            <div className='flex justify-between items-center self-stretch'>
+        <footer className='flex w-full pt-6 pb-14 flex-col items-start gap-6 border-t border-[#F0F0F0]'>
+            <div className='w-[1200px] mx-auto flex justify-between items-center self-stretch'>
                 {/* 좌측: 로고 + 링크 */}
-                <div className='flex w-[390px] items-center gap-[8px]'>
+                <div className='flex w-[390px] items-center gap-2'>
                     <LogoIcon className="w-8 h-8" />
                     <div className='flex items-center'>
-                        <button className='flex h-[44px] py-0 px-[24px] justify-center items-center gap-[8px] text-[#949494] font-pretendard text-[16px] font-medium leading-[140%] hover:underline hover:text-[#707070]'>이용약관</button>
+                        <button className='flex h-11 px-6 justify-center items-center gap-2 text-[#949494] font-pretendard text-base font-medium leading-[140%] hover:underline hover:text-[#707070]'>이용약관</button>
+                        {/* divider */}
                         <div className='w-0 h-[20.308px]'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="2" height="21" viewBox="0 0 2 21" fill="none">
-                                <path d="M0.904297 0.428467V20.7361" stroke="#F0F0F0" strokeWidth="1" />
+                                <path d="M0.691406 0.430176V20.7378" stroke="#F0F0F0" strokeWidth="1" />
                             </svg>
                         </div>
-                        <button className='flex h-[44px] py-0 px-[24px] justify-center items-center gap-[8px] text-[#949494] font-pretendard text-[16px] font-medium leading-[140%] hover:underline hover:text-[#707070]'>개인정보처리방침</button>
+                        <button className='flex h-11 px-6 justify-center items-center gap-2 text-[#949494] font-pretendard text-base font-medium leading-[140%] hover:underline hover:text-[#707070]'>개인정보처리방침</button>
                     </div>
                 </div>
                 {/* 우측: 카피라이트 */}
-                <div className='text-[#CCC] text-right font-montserrat text-[16px] font-medium'>
-                    Copyright © PACKUP Co. All rights reserved
-                </div>
+                <div className='text-[#CCC] text-right font-montserrat text-base font-medium leading-[normal]'>Copyright © PACKUP Co. All rights reserved</div>
             </div>
         </footer>
     );
