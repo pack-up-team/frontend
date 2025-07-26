d1fa30be7fc0f30c551c8742f75d967d41ca3945
codenamewont <codenamewont@gmail.com>
2025-07-26 20:34:36 +0900

    feat: header 컴포넌트 storybook 생성

---
+++ src/components/Header/Header.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Header from '.';
+
+const meta: Meta<typeof Header> = {
+    title: 'Components/Header',
+    component: Header,
+    decorators: [
+        (Story) => (
+        <div className='bg-[#FAFAFA] w-full pb-5'>
+            <Story />
+        </div>
+        ),
+    ],
+    argTypes: {
+        pageType: {
+            control: { type: 'select' },
+            options: ['default', 'landing', 'login', 'signup'],
+            description: '헤더의 페이지 타입',
+        },
+        notificationCount: {
+            control: { type: 'number' },
+            description: '알림 개수',
+        },
+    },
+    args: {
+        pageType: 'default',
+        notificationCount: 0,
+    },
+};
+
+export default meta;
+type Story = StoryObj<typeof Header>;
+
+export const Default: Story = {
+    render: (args) => <Header {...args} />,
+};
