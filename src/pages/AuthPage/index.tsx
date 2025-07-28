237e0ed22d2de724cb1dafa5af8ac15ac603dd46
codenamewont <codenamewont@gmail.com>
2025-07-28 11:31:38 +0900

    feat: auth 페이지 구성

--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,5 +1,14 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 const AuthPage = () => {
-    return <div>auth page</div>;
+    return (
+        <div className="w-full bg-[#FAFAFA]">
+            <Header />
+            <main className="pt-[76px]"></main>
+            <Footer />
+        </div>
+    );
 };
 
 export default AuthPage;
