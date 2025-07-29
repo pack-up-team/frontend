e81c10240ac179327f92de865d37ae90409ba8a9
codenamewont <codenamewont@gmail.com>
2025-07-29 14:17:08 +0900

    feat: env 환경변수 설정

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+
 const instance = axios.create({
-    baseURL: 'http://15.164.93.209:8080', // 서버 주소 설정
+    baseURL,
     withCredentials: false,
 });
 
