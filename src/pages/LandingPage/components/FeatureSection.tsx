68b97e935d7085956bc4dc1276bf0da4774cd201
codenamewont <codenamewont@gmail.com>
2025-07-31 01:06:19 +0900

    feat: 랜딩페이지 feature section 작업

---
+++ src/pages/LandingPage/components/FeatureSection.tsx
@@ -0,0 +1,43 @@
+import { CorrectIcon } from "../../../assets";
+
+type FeatureSectionProps = {
+    title: string;
+    description: string;
+    checklist: string[];
+    image: string;
+    reverse?: boolean; // 레이아웃 순서
+};
+
+const FeatureSection: React.FC<FeatureSectionProps> = ({
+    title,
+    description,
+    checklist,
+    image,
+    reverse = false,
+}) => {
+    return (
+        <section className={`flex w-[1200px] items-center gap-[24px] ${reverse ? 'flex-row-reverse' : ''}`}>
+            {/* 텍스트 영역 */}
+            <div className="flex flex-col items-start gap-[54px] flex-[1_0_0]">
+                <div className="flex flex-col items-start gap-[16px] self-stretch">
+                    <h3 className="self-stretch text-[#141414] font-pretendard text-[48px] font-bold leading-normal">{title}</h3>
+                    <p className="text-[#707070] font-pretendard text-[18px] font-medium leading-[140%]">{description}</p>
+                </div>
+                <div className="flex w-[353px] flex-col items-start gap-[16px]">
+                    {checklist.map((item, idx) => (
+                        <div key={idx} className="flex items-center gap-[16px] self-stretch">
+                            <CorrectIcon className="w-6 h-6" />
+                            <span className="text-[#141414] text-center font-pretendard text-[22px] font-medium leading-[140%]">
+                                {item}
+                            </span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {/* 이미지 교체 필요 */}
+            <div className="w-[700px] h-[540px] flex-shrink-0 rounded-[12px] bg-cover" style={{ backgroundImage: `url(${image})` }}></div>
+        </section>
+    );
+};
+
+export default FeatureSection;
