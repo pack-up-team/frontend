e03166e39a064cce935b07a434e72177dfe570be
codenamewont <codenamewont@gmail.com>
2025-07-31 17:26:16 +0900

    refactor: 배너 접근성 향상

--- src/pages/LandingPage/components/BannerSection.tsx
+++ src/pages/LandingPage/components/BannerSection.tsx
@@ -5,10 +5,10 @@ const BannerSection = () => {
 
     return (
         <section className="flex items-center justify-center w-full max-w-[1200px] h-[260px] flex-shrink-0 rounded-[12px] bg-cover"
-        style={{ backgroundImage: "url('/banner.svg')" }}>
+        style={{ backgroundImage: "url('/banner.svg')" }} role="banner" aria-label="나만의 템플릿 만들기 배너">
             <div className="flex w-[245px] flex-col items-center gap-[33px]">
                 <div className="flex flex-col items-center gap-[16px] self-stretch">
-                    <span className="self-stretch text-white/70 text-center font-montserrat text-[18px] font-semibold leading-[140%] uppercase">Pack up</span>
+                    <p className="self-stretch text-white/70 text-center font-montserrat text-[18px] font-semibold leading-[140%] uppercase">Pack up</p>
                     <h3 className="text-white font-pretendard text-[26px] font-bold leading-normal">지금 바로 시작해보세요!</h3>
                 </div>
                 <button type="button" onClick={() => navigate('/auth')} className="flex w-[240px] h-[44px] px-[24px] py-[8px] justify-between items-center rounded-[4.235px] bg-white">
