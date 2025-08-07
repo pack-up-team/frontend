8c6a8b3cfc483eba07191b0aa2c1d298dc86f484
codenamewont <codenamewont@gmail.com>
2025-08-08 04:14:28 +0900

    feat: 템플릿 상태관리 store 구현

---
+++ src/stores/templateDetailStore.ts
@@ -0,0 +1,51 @@
+import { create } from "zustand";
+
+interface StepObject {
+    cateNo: number;
+    objNo: number;
+    objNm: string;
+    locNum: number;
+}
+
+interface StepText {
+    text: string;
+    stepTextX: number;
+    stepTextY: number;
+}
+
+interface Step {
+    step: number;
+    stepX: number;
+    stepY: number;
+    stepObjList: StepObject[];
+    stepTextList: StepText[];
+}
+
+interface TemplateData {
+    templateNo: number;
+    templateNm: string;
+    templateCateNo: number; // 템플릿 카테고리 번호
+    userNo: number;
+    userId: string;
+    imgFile: string;
+    stepsList: Step[];
+
+    alarmDt?: string | null;
+    repeatType?: string | null;
+    alarmRepeatDay?: string[] | null;
+    alarmTime?: string | null;
+}
+
+interface TemplateState {
+    templateData: TemplateData | null;
+    setTemplateData: (data: TemplateData) => void;
+    clearTemplate: () => void;
+}
+
+export const useTemplateStore = create<TemplateState>((set) => ({
+    templateData: null,
+
+    setTemplateData: (data) => set({ templateData: data }),
+
+    clearTemplate: () => set({ templateData: null }),
+}));
