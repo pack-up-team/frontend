e861c6ac89d215b89bd28448728044c6065d8a1b
codenamewont <codenamewont@gmail.com>
2025-08-11 12:01:25 +0900

    chore: 들여쓰기 변경

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,10 +6,10 @@ import TemplateViewPage from '../pages/TemplateViewPage';
 import TemplateEditPage from '../pages/TemplateEditPage';
 
 export const routes = [
-  { path: '/', element: <LandingPage /> },
-  { path: 'auth', element: <AuthPage /> },
-  { path: 'auth/sns-callback', element: <SnsCallback /> }, // SNS 콜백 라우트 추가
-  { path: 'dashboard', element: <DashboardPage /> },
-  { path: 'template/:id', element: <TemplateViewPage /> },
-  { path: 'template/:id/edit', element: <TemplateEditPage /> },
+    { path: '/', element: <LandingPage /> },
+    { path: 'auth', element: <AuthPage /> },
+    { path: 'auth/sns-callback', element: <SnsCallback /> }, // SNS 콜백 라우트 추가
+    { path: 'dashboard', element: <DashboardPage /> },
+    { path: 'template/:id', element: <TemplateViewPage /> },
+    { path: 'template/:id/edit', element: <TemplateEditPage /> },
 ];
